c0fed9e006a969b9dcd181427ca38d038cc8b42a
Kristoffer <krisrand@stud.ntnu.no>
2015-10-15 14:49:23 +0200

    Created all pages and added art + video from Google Drive

---
+++ dwarfheim.php
@@ -0,0 +1,8 @@
+<?php $title = ""; ?>
+<?php include_once("modules/head.php") ?>
+<?php include_once("modules/nav.php"); ?>
+
+<!-- CONTENT HERE -->
+
+
+<?php include_once "modules/footer.php"; ?>
---
+++ js/lp_cookie.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Kristoffer on 15/10/2015.
+ */
