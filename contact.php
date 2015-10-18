f7656125d9f3f87c179fa682d400b7dd29ab0874
Kristoffer <krisrand@stud.ntnu.no>
2015-10-18 18:34:33 +0200

    create contact form

--- contact.php
+++ contact.php
@@ -4,5 +4,11 @@
 
 <!-- CONTENT HERE -->
 
+<form action="contact.php">
+    <input type="text" placeholder="Subject" required><br/>
+    <textarea placeholder="Message" required></textarea><br/>
+    <input type="email" placeholder="E-mail" required><br/>
+    <button type="submit">Send</button>
+</form>
 
 <?php include_once "modules/footer.php"; ?>
