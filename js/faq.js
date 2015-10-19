1c5a2c9373d0a2a3e87ea6201f911471d969bc22
Kristoffer <krisrand@stud.ntnu.no>
2015-10-19 14:45:19 +0200

    Must actually click on the question box

--- js/faq.js
+++ js/faq.js
@@ -10,18 +10,23 @@ function getEventTarget(e) {
 var ul = document.getElementById('faq');
 ul.onclick = function (event) {
     var target = getEventTarget(event);
-    if(target.tagName == "SPAN"){target = target.parentNode;}
-
-    var arrow = target.getElementsByTagName("span")[0];
-    var child = target.getElementsByTagName("ul")[0];
-    if(arrow.className.indexOf("right") > -1){
-        arrow.className = arrow.className.replace("right", "down");
-        arrow.innerHTML = "&#x25BC;";
-    }else{
-        arrow.className = arrow.className.replace("down", "right");
-        arrow.innerHTML = "&#x25BA;";
+    if (target.id == "faq") {
+        return;
     }
+        if (target.tagName == "SPAN") {
+            target = target.parentNode;
+        }
 
-    child.style.visibility = child.style.visibility == "hidden" ? "visible" : "hidden";
-    child.style.display = child.style.display == "none" ? "block" : "none";
+        var arrow = target.getElementsByTagName("span")[0];
+        var child = target.getElementsByTagName("ul")[0];
+        if (arrow.className.indexOf("right") > -1) {
+            arrow.className = arrow.className.replace("right", "down");
+            arrow.innerHTML = "&#x25BC;";
+        } else {
+            arrow.className = arrow.className.replace("down", "right");
+            arrow.innerHTML = "&#x25BA;";
+        }
+
+        child.style.visibility = child.style.visibility == "hidden" ? "visible" : "hidden";
+        child.style.display = child.style.display == "none" ? "block" : "none";
 };
