d64d358519eeb55d5496ed2fcb8fee654eb04a2a
Kristoffer <kra1992@hotmail.com>
2015-10-21 22:34:29 +0200

    Centered contact form

--- contact.php
+++ contact.php
@@ -4,9 +4,9 @@
 
 <!-- CONTENT HERE -->
 <div class="row">
-    <div class="offset-by-three six columns">
+    <div style="margin:0 auto;max-width: 433px;">
         <header>
-            <h1>Contact us</h1>
+            <h1 style="text-align: center;">Contact us</h1>
         </header>
         <section>
             <p style="color:#E86700">Check out our frequently asked questions <a
@@ -18,40 +18,28 @@
     </div>
 </div>
 
-<div class="row" style="margin-top:50px;">
-    <header class="offset-by-two nine columns">
+
+    <header class="row" style="margin-top:50px;">
         <h2 style="text-align: center">Send a message to to the DwarHeim Team</h2>
     </header>
-</div>
+
 <div class="row">
-    <div class="offset-by-three six columns">
+    <div style="max-width:500px; margin: 0 auto;">
         <form action="contactform.php" method="post">
             <div class="row">
-                <div class="three columns">
                     <label for="name">Name: </label>
-                </div>
-                <div class="nine columns">
                     <input type="text" placeholder="Your Name" name="name" id="name" required>
-                </div>
             </div>
             <div class="row">
-                <div class="three columns">
                     <label for="email">Email: </label>
-                </div>
-                <div class="nine columns">
                     <input type="email" placeholder="Your Email" name="email" id="email" required>
-                </div>
             </div>
             <div class="row">
-                <div class="three columns">
                     <label for="message">Message: </label>
-                </div>
-                <div class="nine columns">
                     <textarea placeholder="Message" id="message" name="message" required></textarea>
-                </div>
             </div>
 
-            <div class="btn-center offset-by-three nine columns">
+            <div class="btn-center">
                 <button class="btn" type="submit" style="text-align: center">Send</button>
             </div>
         </form>
