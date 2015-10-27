d727eb3a5c6561993e7dd919797e68f65d139665
larsfk <larsklingen@gmail.com>
2015-10-27 16:25:02 +0100

    Added css to gallery, deleted footer-image from art-folder, added js for gallery, added path for css in paths.php, added divs and img in media.php

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,59 @@
 /**
  * Created by Kristoffer on 15/10/2015.
  */
+
+var myImage = document.getElementById("myPhoto");
+
+var leftHolder = document.getElementById("leftHolder");
+leftHolder.addEventListener("click",function(){
+    changeImage(true);
+});
+//leftHolder.addEventListener("mouseover",show);
+
+var rightHolder = document.getElementById("rightHolder");
+rightHolder.addEventListener("click",function(){
+    changeImage(false);
+});
+//rightHolder.addEventListener("mouseover",show);
+
+var imageIndex = 0;
+var imageArray = ["images/art/Concept 01.jpg",
+"images/art/Concept 02.jpg",
+"images/art/Concept 03.jpg",
+"images/art/DwarfHeim 1.jpg",
+"images/art/DwarfHeim 2 Blue.jpg",
+"images/art/DwarfHeim 3.jpg",
+"images/art/DwarfHeim 4.jpg",
+"images/art/DwarfHeim 5 Blue.jpg",
+"images/art/DwarfHeim 6 Blue.jpg",
+"images/art/DwarfHeim 7 Grass.jpg",
+"images/art/DwarfHeim 8 INIT.jpg"];
+myPhoto.src = imageArray[0];
+
+
+function changeImage(left){
+    if(left){
+        imageIndex--;
+    }else{
+        imageIndex++;
+    }
+    console.log(imageIndex);
+
+    if(imageIndex >= imageArray.length){
+        imageIndex = 0;
+    }
+    else if(imageIndex < 0){
+        imageIndex = imageArray.length - 1;
+    }
+
+    myPhoto.src = imageArray[imageIndex];
+
+}
+
+function show(){
+    console.log("hover");
+    this.style.visibility = "visible";
+}
+
+
+
--- media.php
+++ media.php
@@ -1,8 +1,21 @@
 <?php $title = "Media"; ?>
+<?php $head = '<link type="text/css" rel="stylesheet" href="css/media.css" >'?>
 <?php include_once("modules/head.php") ?>
 <?php include_once("modules/nav.php"); ?>
 
+
+
 <!-- CONTENT HERE -->
+<div id="slideshow">
+    <img id="myPhoto" src="">
+    <div class="holder" id="leftHolder">
+        <i class="fa fa-arrow-circle-o-left fa-5x"></i>
+    </div>
+    <div class="holder" id="rightHolder">
+        <i class="fa fa-arrow-circle-o-right fa-5x"></i>
+    </div>
 
+</div>
 
+<script src="<?php echo $js['gallery']?>"></script>
 <?php include_once "modules/footer.php"; ?>
