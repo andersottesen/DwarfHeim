e7208292893644ca1a9342a325ce647b80679e29
marianaspas <mariaasp@stud.ntnu.no>
2015-11-10 13:24:39 +0100

    drop-down menu til mobil

--- modules/nav.php
+++ modules/nav.php
@@ -1,29 +1,66 @@
 <nav class="container">
     <div id="top-nav">
+        <ul class="navbar">
+            <li><a href="<?php echo $page["homepage"] ?>">Home</a></li>
+            <li><a href="<?php echo $page["media"] ?>">Media</a>
+                <ul>
+                    <li><a href="<?php echo $page["videos"] ?>">Videos</a></li>
+                    <li><a href="<?php echo $page["images"] ?>">Images</a></li>
+                </ul>
+            </li>
+            <li><a href="<?php echo $page["dwarfheim"] ?>">The Game</a></li>
+            <li><a href="<?php echo $page["about"] ?>">The Crew</a>
+                <ul>
+                    <li><a href="<?php echo $page["contact"] ?>"> Contact </a></li>
+                </ul>
+            </li>
+            <li><a href="<?php echo $page["faq"] ?>">FAQ</a></li>
+            <li><a href="<?php echo $page["investor"] ?>">Investors</a></li>
+        </ul>
 
-    <ul class="navbar">
-        <li><a href="<?php echo $page["homepage"] ?>">Home</a></li>
-        <li><a href="<?php echo $page["media"] ?>">Media</a>
-            <ul>
-                <li><a href="<?php echo $page["videos"] ?>">Videos</a></li>
-                <li><a href="<?php echo $page["images"] ?>">Images</a></li>
-            </ul>
-        </li>
-        <li><a href="<?php echo $page["dwarfheim"] ?>">The Game</a></li>
-        <li><a href="<?php echo $page["about"] ?>">The Crew</a>
-            <ul>
-                <li><a href="<?php echo $page["contact"] ?>"> Contact </a></li>
-            </ul>
-        </li>
-        <li><a href="<?php echo $page["faq"] ?>">FAQ</a></li>
-        <li><a href="<?php echo $page["investor"] ?>">Investors</a></li>
-    </ul>
+
+        <ul class="mobile-menu">
+            <li><a><img id="drop-menu" src="<?php echo $root.'/images/art/small/DwarfHeim 8 INIT small.jpeg'; ?>"> Menu </a></li>
+                <ul id="content">
+                    <li><a href="<?php echo $page["homepage"] ?>">Home</a></li>
+                    <li><a href="<?php echo $page["media"] ?>">Media</a></li>
+                    <li><a href="<?php echo $page["dwarfheim"] ?>">The Game</a></li>
+                    <li><a href="<?php echo $page["about"] ?>">The Crew</a></li>
+                    <li><a href="<?php echo $page["faq"] ?>">FAQ</a></li>
+                    <li><a href="<?php echo $page["investor"] ?>">Investors</a></li>
+                </ul>
+        </ul>
     </div>
     <img id="header-image" style="margin-top:-21px;" src="<?php echo $root.'/images/Navpic.jpg'; ?>" width="100%" alt="">
+
+
 </nav>
 <script>
+
+
+    $(document).ready(function () {
+        $('.mobile-menu').hover(
+            function () {
+                $('#content', this).fadeIn(200);
+            },
+            function () {
+                $('#content', this).fadeOut(200);
+            }
+        );
+    });
+
     $(document).ready(function () {
         $('.navbar li').hover(
+            function () {
+                $('ul', this).fadeIn(200);
+            },
+            function () {
+                $('ul', this).fadeOut(200);
+            }
+        );
+    });
+    $(document).ready(function () {
+        $('.menu li').hover(
             function () {
                 $('ul', this).fadeIn(200);
             },
